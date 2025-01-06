ca76bdce355b472b690a112d954596b428a9aea6
k-taro <my.address.d@gmail.com>
2025-01-07 00:00:15 +0900

    Add Introduction component and update index.tsx to include it.

--- src/app/_components/index.tsx
+++ src/app/_components/index.tsx
@@ -1,8 +1,8 @@
 import { Footer } from "@/components/ui/footer/footer";
 import { Header } from "@/components/ui/header/header";
 import { HeroCarousel } from "./heroCarousel";
-import { News } from "./news";
 import { Festivals } from "./festivals";
+import { Introduction } from "./introduction";
 
 export const Index = () => {
   return (
@@ -10,8 +10,9 @@ export const Index = () => {
       <Header />
       <main className="pb-40">
         <HeroCarousel />
-        <div className="mt-20" />
-        <News />
+        <div className="mt-40" />
+        {/* <News /> */}
+        <Introduction />
         <div className="mt-80" />
         <Festivals />
       </main>
---
+++ src/app/_components/introduction.tsx
@@ -0,0 +1,20 @@
+export const Introduction = () => {
+  return (
+    <section className="px-5 max-w-7xl mx-auto">
+      <h2 className="text-3xl text-white text-center">INTRODUCTION</h2>
+      <div className="mt-10" />
+      <p className="text-white text-left md:text-center !leading-10 text-sm md:text-base">
+        心を揺さぶる音楽と美酒に酔う 「
+        <span className="font-bold">大人の文化祭</span>」
+        <br />
+        華やかに輝く仲間のステージが特別な夜を彩ります。
+        <br />
+        お酒を片手に、音楽に身を委ね、日常を忘れる贅沢なひとときを過ごしませんか？
+        <br />
+        仲間と笑い合い、胸が熱くなる瞬間を共有できるのは、大人だからこそ。
+        <br />
+        音楽とお酒が織りなす特別な文化祭を、ぜひお楽しみください。
+      </p>
+    </section>
+  );
+};
